e0333609d27613436c561dacebc229ebe26bbc72
Sarah <sarah.tiefert@htwg-konstanz.de>
2020-11-12 14:29:43 +0100

    correction A1

--- Clojure/src/SpKo_B2/A1.cljs
+++ Clojure/src/SpKo_B2/A1.cljs
@@ -1,10 +1,5 @@
 (ns SpKo-B2.A1)
 (def lst '(1 2 3 -4 -5 6 -7 8 9))
 (defn pos [x] (> x 0))
-((defn posF
-  "get all numbers larger than 0"
-  [lst]
-  (filer pos lst)))
-
-
-defn posR [lst] (if (empty? lst) nil (if (pos (first lst)) (concat (list(first lst)) (posR(rest lst))) (concat nil (posR(rest lst))))))
+(defn posF [lst] (filter pos lst))
+(defn posR [lst] (if (empty? lst) nil (if (pos (first lst)) (concat (list(first lst)) (posR(rest lst))) (concat nil (posR(rest lst))))))
