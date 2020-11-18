f5dc2f83ec976b89983fb7ae191370186e5450a4
Sarah <sarah.tiefert@htwg-konstanz.de>
2020-11-18 20:37:54 +0100

    finished A7 a can't find a solution to A7 b

--- Clojure/src/SpKo_B2/A7.cljs
+++ Clojure/src/SpKo_B2/A7.cljs
@@ -1,3 +1,2 @@
 (ns SpKo-B2.A7)
-(defn elA [x lst] (if(empty? lst) nil (if (= (first lst) x) (el x (rest lst)) (concat (list(first lst)) (el x (rest lst))))))
-
+(defn el [x lst] (filter #(not= % x) lst))
