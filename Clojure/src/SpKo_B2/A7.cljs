fb6d5d86cc58fef0c319bee0d9cc7596e4aae5f4
Sarah <sarah.tiefert@htwg-konstanz.de>
2020-11-19 02:25:04 +0100

    finished A7

--- Clojure/src/SpKo_B2/A7.cljs
+++ Clojure/src/SpKo_B2/A7.cljs
@@ -1,2 +1,6 @@
 (ns SpKo-B2.A7)
 (defn el [x lst] (filter #(not= % x) lst))
+(defn elB [x lst] (loop[x x lst (el x lst) res '()] (if (empty? lst) res (recur x (rest lst) (if (list? (first lst)) (concat res (list (el 2 (first lst)))) )))))
+
+
+(defn elC [x lst] (loop[x x lst (el x lst) res '()] (if (empty? lst) res (recur x (rest lst) (if (list? (first lst)) (concat res (list (elB 2 (first lst)))) )))))
